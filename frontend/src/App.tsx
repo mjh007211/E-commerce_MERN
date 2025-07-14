05c1fd093ae2e6a4f21fb418e6f6b50187ef8bfb
Mohammed Othman <m.othman1198@gmail.com>
2025-07-14 17:34:52 +0300

    install React routers + iinstall MUI + create HomePage + create Navbar compenent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
